ff899bafaf84ab080df5fc1bb782e1f3562dad08
Amanda Morris <amandafmorris@gmail.com>
2017-03-20 20:26:46 +0000

    Detecting cycles done

--- detecting-cycles.py
+++ detecting-cycles.py
@@ -1,3 +1,14 @@
+def has_cycles(head):
+    visited = set([])
+    current = head
+    while current.next:
+        if current in visited:
+            return True
+        visited.add(current)
+        current = current.next
+    return False
+
+
 # A linked list is said to contain a cycle if any node is visited more than once while traversing the list.
 
 # Complete the function provided in the editor below. It has one parameter: a pointer to a Node object named that points to the head of a linked list. Your function must return a boolean denoting whether or not there is a cycle in the list. If there is a cycle, return true; otherwise, return false.
