733586620ab8f87dcfbc4183af4ca01c57e838f2
Amanda Morris <amandafmorris@gmail.com>
2017-03-20 20:53:10 +0000

    Added cache for already-computed values

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,14 @@
 def fibonacci(n):
     # Write your code here.
+    cache = {0:0, 1:1}
     if n == 0:
         return 0
     if n == 1:
         return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    if n not in cache:
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+    return cache[n]
+
 
 print fibonacci(10)
 # fibonacci(6)
